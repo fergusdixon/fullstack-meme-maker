b5fd354ab1d9d87c950e374fad3a86e7d0210588
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2021-04-19 18:11:04 +0200

    fix tests

--- meme-api/app/api/api_v1/endpoints/memes.py
+++ meme-api/app/api/api_v1/endpoints/memes.py
@@ -8,7 +8,7 @@ from app.core.config import base_images
 router = APIRouter()
 
 
-@router.get("/", response_model=List[schemas.Image])
+@router.get("", response_model=List[schemas.Image])
 def get_image_list() -> Any:
     """
     Gets the list of images to generate memes from
