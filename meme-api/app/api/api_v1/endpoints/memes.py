78ed0d5146330ce3a4cd6d04d6b27e6c7d13bf4e
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2021-04-19 18:01:33 +0200

    fixes

--- meme-api/app/api/api_v1/endpoints/memes.py
+++ meme-api/app/api/api_v1/endpoints/memes.py
@@ -1,6 +1,6 @@
 from typing import Any, List
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
 from app import schemas
 from app.core.config import base_images
@@ -9,18 +9,9 @@ router = APIRouter()
 
 
 @router.get("/", response_model=List[schemas.Image])
-async def get_image_list() -> Any:
+def get_image_list() -> Any:
     """
     Gets the list of images to generate memes from
     """
     with open("app/assets/base_meme_definition.json") as image_list:
         return list(base_images.values())
-
-
-@router.post("/", responses={200: {"content": {"image/png": {}}, "description": "The generated meme"}})
-async def create_meme(meme: schemas.Meme) -> Any:
-    """ "
-    Create a meme from a base image
-    """
-    if not base_images.get(str(meme.uuid)):
-        raise HTTPException(status_code=404, detail=f"Image {meme.uuid} not found")
