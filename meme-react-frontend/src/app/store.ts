745bb2550063f4ff36ca4120cecd17be8aa2d145
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2021-04-20 19:00:19 +0200

    cleanup & frontend testing

--- meme-react-frontend/src/app/store.ts
+++ meme-react-frontend/src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import memesReducer from '../features/memes/memesSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     memes: memesReducer,
   },
 });
