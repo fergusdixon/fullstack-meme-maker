953f2057da40abd66324586375aef9f9db049dfc
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2021-04-21 18:12:43 +0200

    formatting

--- meme-api/app/core/config.py
+++ meme-api/app/core/config.py
@@ -6,7 +6,11 @@ from pydantic import AnyHttpUrl, BaseSettings, validator
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost", "http://localhost:3000", "http://fulls-loadb-16qur6ndc2l03-1254964837.eu-west-2.elb.amazonaws.com"]
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
+        "http://localhost",
+        "http://localhost:3000",
+        "http://fulls-loadb-16qur6ndc2l03-1254964837.eu-west-2.elb.amazonaws.com",
+    ]
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
