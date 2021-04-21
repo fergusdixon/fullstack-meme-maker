caa4e3bf1532c53247f7987a1f8201c72786b761
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2021-04-21 12:35:04 +0200

    delete some comments

--- meme-react-frontend/src/features/memes/memesSlice.ts
+++ meme-react-frontend/src/features/memes/memesSlice.ts
@@ -41,7 +41,7 @@ const initialState: MemesState = {
 };
 
 // The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
+// can be dispatched like a regular action: `dispatch(fetchMemesFromAPI())`. This
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched. Thunks are
 // typically used to make async requests.
@@ -60,20 +60,12 @@ export const memesSlice = createSlice({
     // The `reducers` field lets us define reducers and generate associated actions
     reducers: {
         chooseMeme: (state, action: PayloadAction<number>) => {
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the Immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
             const newIndex = action.payload;
             if (newIndex < state.baseMemes.length && state.baseMemes[newIndex].enabled) {
                 state.selectedMemeIndex = newIndex;
             }
         },
         updateTextFieldValue: (state, action: PayloadAction<UpdateTextfieldEvent>) => {
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the Immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
             state.baseMemes[state.selectedMemeIndex].text_fields[action.payload.textFieldIndex].value = action.payload.value
         },
     },
